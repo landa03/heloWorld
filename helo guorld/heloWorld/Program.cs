136e28e9c9daf70fe75ab5497cb797d59db8ba03
landa03 <60487799+landa03@users.noreply.github.com>
2020-02-15 19:28:09 -0700

    Add files via upload

---
+++ helo guorld/heloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace helo_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Niga!");
+        }
+    }
+}
